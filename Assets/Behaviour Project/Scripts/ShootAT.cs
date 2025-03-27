ea55bd997e03828e74892569060323e1a94e753f
InfiniteClock <156016040+InfiniteClock@users.noreply.github.com>
2025-03-27 15:42:44 -0400

    Week 10 Telegraphing Complete

--- Assets/Behaviour Project/Scripts/ShootAT.cs
+++ Assets/Behaviour Project/Scripts/ShootAT.cs
@@ -1,10 +1,17 @@
 using NodeCanvas.Framework;
 using ParadoxNotion.Design;
+using UnityEngine;
 
 
 namespace NodeCanvas.Tasks.Actions {
 
 	public class ShootAT : ActionTask {
+
+		
+		public BBParameter<Transform> target;
+        public GameObject projectile;
+		[Tooltip("How far in degrees can a shot fire off target?")]
+        public float shotConeWidth;
 
 		//Use for initialization. This is called only once in the lifetime of the task.
 		//Return null if init was successfull. Return an error string otherwise
@@ -16,7 +23,14 @@ namespace NodeCanvas.Tasks.Actions {
 		//Call EndAction() to mark the action as finished, either in success or failure.
 		//EndAction can be called from anywhere.
 		protected override void OnExecute() {
-			EndAction(true);
+            GameObject shot = GameObject.Instantiate(projectile);
+            shot.transform.position = agent.transform.position;
+
+            Vector3 directionToTarget = (target.value.position - shot.transform.position).normalized;
+            shot.transform.forward = directionToTarget;
+
+            float randomAngle = Random.Range(-shotConeWidth, shotConeWidth);
+			shot.transform.Rotate(0, randomAngle, 0);
 		}
 
 		//Called once per frame while the action is active.

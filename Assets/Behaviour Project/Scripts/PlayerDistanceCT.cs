828b81fc7b870b1daf552f342a19b1cd892bbaf8
InfiniteClock <156016040+InfiniteClock@users.noreply.github.com>
2025-04-04 10:46:43 -0400

    Pre-compliance Report

--- Assets/Behaviour Project/Scripts/PlayerDistanceCT.cs
+++ Assets/Behaviour Project/Scripts/PlayerDistanceCT.cs
@@ -1,10 +1,14 @@
 using NodeCanvas.Framework;
 using ParadoxNotion.Design;
+using UnityEngine;
 
 
 namespace NodeCanvas.Tasks.Conditions {
 
 	public class PlayerDistanceCT : ConditionTask {
+
+		public BBParameter<Transform> target;
+		public float distanceThreshold;
 
 		//Use for initialization. This is called only once in the lifetime of the task.
 		//Return null if init was successfull. Return an error string otherwise
@@ -25,7 +29,8 @@ namespace NodeCanvas.Tasks.Conditions {
 		//Called once per frame while the condition is active.
 		//Return whether the condition is success or failure.
 		protected override bool OnCheck() {
-			return true;
+			float distToTarget = Vector3.Distance(agent.transform.position, target.value.position);
+			return distToTarget < distanceThreshold;
 		}
 	}
 }
---
+++ Assets/Behaviour Project/Scripts/StrafeAT.cs
@@ -0,0 +1,71 @@
+using NodeCanvas.Framework;
+using ParadoxNotion.Design;
+using UnityEngine;
+
+
+namespace NodeCanvas.Tasks.Actions {
+
+	public class StrafeAT : ActionTask {
+
+		public BBParameter<Vector3> acceleration;
+		public BBParameter<bool> clockwise;
+		public BBParameter<Transform> target;
+		public float steeringAcceleration;
+		public float boundaryRange;
+        public Vector3 mapCenter;
+		public float rotationDirectionOffset;
+        //Use for initialization. This is called only once in the lifetime of the task.
+        //Return null if init was successfull. Return an error string otherwise
+        protected override string OnInit() {
+			return null;
+		}
+
+		//This is called once each time the task is enabled.
+		//Call EndAction() to mark the action as finished, either in success or failure.
+		//EndAction can be called from anywhere.
+		protected override void OnExecute() {
+			
+			Vector3 dirToCenter = mapCenter - agent.transform.position;
+			dirToCenter = new Vector3(dirToCenter.x, 0, dirToCenter.z);
+			Vector3 left = Vector3.Cross(dirToCenter, Vector3.up);
+			
+			// Determine if we are strafing towards target or away, with room for error
+			Vector3 dirToTarget = target.value.position - agent.transform.position;
+			Debug.Log(Vector3.Dot(left.normalized, dirToTarget.normalized));
+			if (Vector3.Dot(left.normalized, dirToTarget.normalized) > rotationDirectionOffset)
+				clockwise.value = true;
+            else if (Vector3.Dot(left.normalized, dirToTarget.normalized) < -rotationDirectionOffset)
+				clockwise.value = false;
+
+            if (clockwise.value)
+			{
+				acceleration.value -= left * steeringAcceleration * Time.deltaTime;
+			}
+			else
+			{
+				acceleration.value += left * steeringAcceleration * Time.deltaTime;
+			}
+			// Remain within the boundary circle
+			if (dirToCenter.magnitude > boundaryRange)
+			{
+				acceleration.value += dirToCenter * steeringAcceleration * Time.deltaTime;
+			}
+			EndAction(true);
+		}
+
+		//Called once per frame while the action is active.
+		protected override void OnUpdate() {
+			
+		}
+
+		//Called when the task is disabled.
+		protected override void OnStop() {
+			
+		}
+
+		//Called when the task is paused.
+		protected override void OnPause() {
+			
+		}
+	}
+}
